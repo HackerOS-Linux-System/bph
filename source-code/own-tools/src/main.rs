e69bc2517379943ac6071578710a1c50b0703676
michal92299 <chudybyczek2137@gmail.com>
2026-02-04 20:19:37 +0100

    Update main.rs

--- source-code/own-tools/src/main.rs
+++ source-code/own-tools/src/main.rs
@@ -1,1 +1,14 @@
+use pyo3::prelude::*;
+use pyo3::types::PyModule;
+use std::fs;
+
+fn main() -> PyResult<()> {
+    Python::with_gil(|py| {
+        let code = fs::read_to_string("./main.py").expect("Failed to read main.py");
+        let module = PyModule::from_code(py, &code, "main.py", "main")?;
+        let func = module.getattr("main")?;
+        func.call0()?;
+        Ok(())
+    })
+}
 
